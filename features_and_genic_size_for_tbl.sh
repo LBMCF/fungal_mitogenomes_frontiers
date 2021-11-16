1df49ba0f0cf61f0b91b37b55196d3a77a1aabb1
LBMCF <91095205+LBMCF@users.noreply.github.com>
2021-11-16 14:39:23 -0600

    Update features_and_genic_size_for_tbl.sh

--- features_and_genic_size_for_tbl.sh
+++ features_and_genic_size_for_tbl.sh
@@ -1,5 +1,5 @@
 #This script is part of supplementary documents of "Global Characterization of Fungal Mitogenomes: New Insights on Genomic Diversity and Dynamism of Coding Genes and Accessory Elements"
-#submitted to Frontiers in Microbiology, research topic "Mitochondrial Genomes and Mitochondrion Related Gene Insights to Fungal Evolution"
+#published in Frontiers in Microbiology, research topic "Mitochondrial Genomes and Mitochondrion Related Gene Insights to Fungal Evolution"
 #DOI: 10.3389/fmicb.2021.787283
 #Authors:  Paula L. C. Fonseca, Ruth B. De-Paula, Daniel S. Araújo, Luiz Marcelo R. Tomé, Thairine Mendes-Pereira, Wenderson F. Rodrigues, 
 #Luiz-Eduardo Del-Bem, Eric R. Aguiar, and Aristóteles Góes-Neto
